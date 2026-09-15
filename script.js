cb1ed464a85260ec48e566bfab7c05b446d397be
lqttify <fabreconstanza@gmail.com>
2026-09-15 18:25:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+<script> 
+  const menu = document.getElementById("menu"); 
+  const explorarBTN = document.getElementById("explorarBTN"); 
+  const opcionesBTN = document.getElementById("opcionesBTN"); explorarBTN.addEventListener("click", function() { menu.classList.add("deslizar"); }); opcionesBTN.addEventListener("click", function() { menu.classList.add("deslizar"); }); </script> 
