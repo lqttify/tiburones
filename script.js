9eec88de4662a3632c1a791451758d41bdbf3894
lqttify <fabreconstanza@gmail.com>
2026-09-16 14:56:57 -0300

    button update

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-<script>
     const explorarBTN = document.getElementById("explorarBTN");
     const menu = document.getElementById("menu");
+    const volverBTN = document.getElementById("volverBTN");
 
     explorarBTN.addEventListener("click", function() {
-        menu.style.transform = "translateX(-100%)";
+        menu.style.transform = "translateX(-500px)";
+        volverBTN.style.transition = "opacity 0.6s, transform 0.6s";
+        volverBTN.style.opacity = "1";
+        volverBTN.style.transform = "translateX(0)";
+       
     });
-     </script> 
+
+    volverBTN.addEventListener("click", function() {
+        menu.style.transform = "translateX(0)";
+        volverBTN.style.transition = "opacity 0.6s, transform 0.6s";
+        volverBTN.style.opacity = "0";
+        volverBTN.style.transform = "translateX(500px)";
+        
+    });
