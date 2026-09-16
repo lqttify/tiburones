969b35c4a64cbecd9df896ec585957426ba309a2
lqttify <fabreconstanza@gmail.com>
2026-09-16 12:26:45 -0300

    rewrite script

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-<script> const menu = document.getElementById("menu"); const explorarBTN = document.getElementById("explorarBTN"); const opcionesBTN = document.getElementById("opcionesBTN"); explorarBTN.addEventListener("click", function() { menu.classList.add("deslizar"); }); opcionesBTN.addEventListener("click", function() { menu.classList.add("deslizar"); }); </script> 
+<script>
+    const explorarBTN = document.getElementById("explorarBTN");
+    const menu = document.getElementById("menu");
+
+    explorarBTN.addEventListener("click", function() {
+        menubar.style.transform = "translateX(-100%)";
+    });
+     </script> 
